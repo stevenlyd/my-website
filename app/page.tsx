411e427d86bae26d87e6b6c971f42abc4ebf5d2d
Steven Liu <liuyidong9@gmail.com>
2023-02-28 12:43:04 -0500

    Fixed homepage photo.

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,42 @@
 import SImage from "@/components/SImage";
 
-const fetchHomePagePhoto = () => fetch(`https://steven-pocketbase.fly.dev/api/collections/photos/records?` + new URLSearchParams({
-    page: '1',
-    perPage: '1',
-    sort: '-created',
-}), {
-    method: 'get',
-    cache: 'force-cache',
-    next: {
-        revalidate: 60,
-    }
-}).then((res) => res.json())
+// const fetchHomePagePhoto = () => fetch(`https://steven-pocketbase.fly.dev/api/collections/photos/records?` + new URLSearchParams({
+//     page: '1',
+//     perPage: '1',
+//     sort: '-created',
+// }), {
+//     method: 'get',
+//     cache: 'force-cache',
+//     next: {
+//         revalidate: 60,
+//     }
+// }).then((res) => res.json())
 
 export default async function Home() {
-    const data = await fetchHomePagePhoto()
+    // const data = await fetchHomePagePhoto()
     return (
         <div style={{
             display: 'flex',
             justifyContent: 'center',
             marginTop: '50px',
         }}>
-            <SImage alt={data.items[0].id}
-                    src={`https://steven-pocketbase.fly.dev/api/files/photos/${data.items[0].id}/${data.items[0].photo}`}
-                    transition={{
-                        transitionDuration: 800,
-                    }}
-                    style={{
-                        height: 'auto',
-                        width: 'auto',
-                        maxWidth: '100%',
-                        maxHeight: '75vh',
-                        margin: 'auto',
-                        justifySelf: 'center',
-                        boxShadow: '0 10px 20px -4px rgb(0 0 0 / 50%)',
-                    }}
+            <SImage
+                // alt={data.items[0].id}
+                // src={`https://steven-pocketbase.fly.dev/api/files/photos/${data.items[0].id}/${data.items[0].photo}`}
+                alt='Boston'
+                src='https://steven-pocketbase.fly.dev/api/files/tgyzgtr4jl9bqdf/4so2jjjkeff73r8/museum_jg54VZ3NDy.jpg'
+                transition={{
+                    transitionDuration: 800,
+                }}
+                style={{
+                    height: 'auto',
+                    width: 'auto',
+                    maxWidth: '100%',
+                    maxHeight: '75vh',
+                    margin: 'auto',
+                    justifySelf: 'center',
+                    boxShadow: '0 10px 20px -4px rgb(0 0 0 / 50%)',
+                }}
             />
         </div>
     )
