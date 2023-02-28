31ba5b025a9c5d509f10ab42c4edb77b2167b20f
Steven Liu <liuyidong9@gmail.com>
2023-02-28 01:11:41 -0500

    Smaller trip view size.

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -3,8 +3,21 @@ import SImage from "@/components/SImage";
 
 export default function Gallery() {
     return (
-            <SImage src={'https://steven-pocketbase.fly.dev/api/files/tgyzgtr4jl9bqdf/yg8a7yjfx8ztont/crater_edge_2kkwcCRfem.jpg'} alt='gallery' transition={{
+        <SImage
+            src={'https://steven-pocketbase.fly.dev/api/files/tgyzgtr4jl9bqdf/yg8a7yjfx8ztont/crater_edge_2kkwcCRfem.jpg'}
+            alt='gallery'
+            transition={{
                 transitionDuration: 800,
-            }}/>
+            }}
+            style={{
+                height: 'auto',
+                width: 'auto',
+                maxWidth: '100%',
+                maxHeight: '75vh',
+                margin: 'auto',
+                justifySelf: 'center',
+                boxShadow: '0 10px 20px -4px rgb(0 0 0 / 50%)',
+            }}
+        />
     )
 }
