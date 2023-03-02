62df2d657aed9527f506255b2e6f3651751d80a4
Steven Liu <liuyidong9@gmail.com>
2023-03-02 08:34:13 -0500

    Added iphone section in gallery navbar.

--- app/gallery/layout.tsx
+++ app/gallery/layout.tsx
@@ -3,6 +3,7 @@ import {Grid, useMediaQuery} from "@mui/material";
 import React from "react";
 import Trips from "@/components/Trips";
 import {usePathname} from "next/navigation";
+import Link from "next/link";
 
 export default function GalleryLayout({children}: { children: React.ReactNode }) {
     const mobile = !useMediaQuery('(min-width:700px)')
@@ -18,7 +19,30 @@ export default function GalleryLayout({children}: { children: React.ReactNode })
         return (
             <Grid container direction={mobile ? 'column' : 'row'}>
                 <Grid item xs={2}>
+                    <div style={{
+                        position: 'sticky',
+                        top: '120px',
+                        left: '0px',
+                        display: 'flex',
+                        flexDirection: `${mobile ? 'row' : 'column'}`,
+                        justifyContent: `${mobile ? 'space-around' : 'start'}`,
+                        margin: `${mobile ? '0px 0px 25px 0px' : '0px 25px 0px 0px'}`,
+                    }}>
                     <Trips/>
+                        <Link href={'/gallery/iphone'}
+                              style={{
+                                  textDecoration: `${currentPath?.includes('iphone') ? 'underline' : 'none'}`,
+                                  textUnderlineOffset: '8px',
+                                  color: 'inherit',
+                              }}
+                        >
+                            <h3 style={{
+                                zIndex: '1300',
+                            }}>
+                                iPhone
+                            </h3>
+                        </Link>
+                    </div>
                 </Grid>
                 <Grid item
                       xs={10}>
