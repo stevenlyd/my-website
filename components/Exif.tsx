bd38bee19f842a7ca053aef6d08687c87f444b2d
Steven Liu <liuyidong9@gmail.com>
2023-03-02 16:14:33 -0500

    Auto-detect camera type and display location using reverse geocoding.

--- components/Exif.tsx
+++ components/Exif.tsx
@@ -1,11 +1,59 @@
-import {getImageBuffer} from "@/components/myFunctions";
-import {ExifParserFactory} from "ts-exif-parser";
+import { getImageBuffer } from "@/components/myFunctions";
+import { ExifParserFactory } from "ts-exif-parser";
 import ExifDisplay from "@/components/ExifDisplay";
 
-export default async function Exif({url}: { url: string }): Promise<JSX.Element> {
+const fetchPlaceID = (coordinates: string) => fetch(process.env.GOOGLE_API! + '/geocode/json?' + new URLSearchParams({
+    latlng: coordinates,
+    location_type: 'ROOFTOP',
+    result_type: 'establishment|tourist_attraction',
+    key: process.env.GOOGLE_API_KEY!,
+}),
+    {
+        method: 'get',
+        cache: 'force-cache',
+        next: {
+            revalidate: 60,
+        }
+    }).then((res) => res.json())
+
+const fetchPlaceDetail = (placeID: string) => fetch(process.env.GOOGLE_API! + '/place/details/json?' + new URLSearchParams({
+    place_id: placeID,
+    result_type: 'tourist_attraction|establishment',
+    fields: 'vicinity,name',
+    key: process.env.GOOGLE_API_KEY!,
+}), {
+    method: 'get',
+    next: {
+        revalidate: 60,
+    }
+}).then((res) => res.json())
+
+export default async function Exif({ url }: { url: string }): Promise<JSX.Element> {
     const buffer = await getImageBuffer(url)
     const exif = ExifParserFactory.create(buffer).parse()
-    return (
-        <ExifDisplay exif={exif?.tags!}/>
-    )
+    const exifTags = exif?.tags!
+    let placeDetailRes = undefined
+
+    if (exifTags?.GPSLatitude) {
+        const coordinates = `${exifTags.GPSLatitude}, ${exifTags.GPSLongitude}`
+        const placeIDRes = await fetchPlaceID(coordinates)
+        const placeID = placeIDRes?.results[0]?.place_id
+        placeDetailRes = await fetchPlaceDetail(placeID)
+    }
+
+    if (exifTags?.LensMake) {
+        if (exifTags.LensMake === 'Apple') {
+            return (
+                <ExifDisplay exif={exifTags} type='phone' location={placeDetailRes?.result!} />
+            )
+        } else {
+            return (
+                <ExifDisplay exif={exifTags} type='camera' location={placeDetailRes?.result!} />
+            )
+        }
+    } else {
+        return (
+            <ExifDisplay exif={exifTags} type='film' />
+        )
+    }
 }
