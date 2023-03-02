80073af94923b19a9176bf45f7a920bceeca1ef6
Steven Liu <liuyidong9@gmail.com>
2023-03-02 08:30:12 -0500

    Added iphone section content.

--- app/gallery/iphone/page.tsx
+++ app/gallery/iphone/page.tsx
@@ -1,9 +1,22 @@
+import {API} from "@/public/variables";
+import Photos from "@/components/Photos";
 
+const fetchPhotos = () => fetch(`${API}/collections/iphone/records?` + new URLSearchParams({
+    sort: '-created',
+}), {
+    method: 'get',
+    cache: 'force-cache',
+    next: {
+        revalidate: 60,
+    }
+}).then((res) => res.json())
 
 export default async function Iphone() {
+    const data = await fetchPhotos()
+    const photoArr = data.items
     return (
         <>
-            <h1>TEST</h1>
+            <Photos photos={photoArr} layout='grid' />
         </>
     )
 }
