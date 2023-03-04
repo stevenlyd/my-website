a118643d02fd71625c681398a2b37c1bd6bf90c1
Steven Liu <liuyidong9@gmail.com>
2023-03-04 11:32:03 -0500

    Removed material ui from detailed page.

--- components/PhotoDetail.tsx
+++ components/PhotoDetail.tsx
@@ -1,29 +1,13 @@
 import { API } from "@/public/variables"
-import SImage from "./SImage"
 import Exif from "./Exif"
+import styles from "./PhotoDetail.module.scss"
+import Image from 'next/image'
 
 export default function PhotoDetail({ photo }: any) {
     return (
-        <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            width: '100%',
-            maxHeight: 'calc(100vh - 180px - 75px)',
-        }}>
-            <SImage src={`${API}/files/${photo.collectionName}/${photo.id}/${photo.photo}`} alt={photo.title}
-                transition={{ transitionDuration: 800 }}
-                style={{
-                    height: 'auto',
-                    width: 'auto',
-                    maxWidth: '100%',
-                    marginTop: '2vh',
-                    marginBottom: '2vh',
-                    marginLeft: 'auto',
-                    marginRight: 'auto',
-                    justifySelf: 'center',
-                    boxShadow: '0px 10px 20px -4px rgb(0 0 0 / 50%)',
-                }}
+        <div className={styles.imageContainer}>
+            <Image src={`${API}/files/${photo.collectionName}/${photo.id}/${photo.photo}`} alt={photo.title}
+                className={styles.image}
                 width={1500}
                 height={1500}
                 quality={100}
